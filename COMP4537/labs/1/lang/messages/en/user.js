6b048a3142269ff8774e0e979bc4023e8db8a1c9
Inez Yoon <inez3cs@gmail.com>
2025-01-19 23:29:08 -0800

    Add reader

--- COMP4537/labs/1/lang/messages/en/user.js
+++ COMP4537/labs/1/lang/messages/en/user.js
@@ -1,7 +1,10 @@
+// This messages were cleaned up by chatGPT
 window.messages = {
-    msg_notSupported: "Sorry web Storage is not supported!",
-    msg_written: "A piece of data was written in local storage for the key: ",
-
+    msg_notSupported: "Sorry, web storage is not supported!",
+    msg_written: "A piece of data was written in local storage for the key: {key}.",
+    msg_removed: "The data associated with the key: {key} has been removed.",
+    msg_modified: "The data for key: {key} has been updated to: {value}.",
+    msg_error: "An error occurred: {error}. Please try again."
 };
 
 // Export the messages object for use in other files
